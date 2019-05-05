97de8f0870738d78c04b02e6a6726086b277a9f1
Hai Liang Wang <hain@chatopera.com>
2019-05-05 20:53:16 +0800

    Use build type

--- app/t2/run.sh
+++ app/t2/run.sh
@@ -6,16 +6,21 @@
 # constants
 baseDir=$(cd `dirname "$0"`;pwd)
 INSTALL_DIR=$baseDir/../../tmp
+BUILD_TYPE=Debug # [Debug|Release|RelWithDebInfo|MinSizeRel]
+BUILD_DIR=build-$BUILD_TYPE
 # functions
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 cd $baseDir/
-if [ -d build ]; then
-    rm -rf build
+if [ -d $BUILD_DIR ]; then
+    rm -rf $BUILD_DIR
 fi
-mkdir build && cd build
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .. 
+mkdir $BUILD_DIR && cd $BUILD_DIR
+cmake -G "Unix Makefiles" \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    .. 
 make
 ./bin/hello-t2
 
